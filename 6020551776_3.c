8cfd921a30d8157c06a5431abd5181970544c823
pengu1n0 <78530429+pengu1n0@users.noreply.github.com>
2021-02-11 17:13:41 +0700

    ยังไม่ถูก

--- 6020551776_3.c
+++ 6020551776_3.c
@@ -1,11 +1,44 @@
 #include<stdio.h>
-int main()
+void main()
 {
-	int n=3,i;
-	int a[n];
+	int n=3;
+	int a[n],i,j;
 	for(i=0;i<n;i++)
 	scanf("%d",&a[i]);
 	char b[n];
 	for(i=0;i<n;i++)
         scanf("%c",&b[i]);
+        
+    int sum;
+    for(j=0 ; j<=n ; j++)
+    {
+        for(i=0 ; i<n ; i++)
+        {
+            if(a[i]>a[i+1])
+            {
+                sum=a[i];
+                a[i]=a[i+1];
+                a[i+1]=sum;
+            }
+        }
+
+    }
+    
+    int Answer[n];
+    for(i=0 ; i<n ; i++)
+    {
+        if(b[i]=='A')
+        {
+            Answer[i]=a[0];
+        }
+        else if(b[i]=='B')
+        {
+            Answer[i]=a[1];
+        }
+        else if(b[i]=='C')
+        {
+            Answer[i]=a[2];
+        }
+    }
+    printf("%d %d %d",Answer[0],Answer[1],Answer[2]);
 }
